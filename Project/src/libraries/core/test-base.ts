78dbeb50f36b1eedd788228c5fd6fabf44850f1e
QualityOwl <75451637+QualityOwl@users.noreply.github.com>
2023-04-05 10:31:29 -0500

    Initial add. (#1)

---
+++ Project/src/libraries/core/test-base.ts
@@ -0,0 +1,25 @@
+import { Browser, test } from '@playwright/test';
+import { Log } from '../console/log';
+
+const log = new Log();
+
+export class TestBase {
+    async Cleanup(browser: Browser) {
+        const contexts = browser.contexts();
+
+        log.StepDescription("Close browser.");
+        for (const context of contexts) {
+            await context.close();
+        }
+    }
+}
+
+test.beforeAll(async ({ page }) => {
+    log.HeaderText();
+});
+
+test.afterAll(async ({ page }) => {
+    log.FooterText();
+});
+
+export default test;
