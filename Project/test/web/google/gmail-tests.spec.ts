c03a2f77e331431549f7ff238e0f0feafee2a3d2
QualityOwl <75451637+QualityOwl@users.noreply.github.com>
2023-04-06 15:13:28 -0500

    Finalize 'Gmail' test script. (#4)
    * Remove default demo test file.
    * Update 'Gmail' test.

--- Project/test/web/google/gmail-tests.spec.ts
+++ Project/test/web/google/gmail-tests.spec.ts
@@ -3,10 +3,14 @@ import { TestBase } from '../../test-base';
 import { Log } from '../../../src/libraries/console/log';
 import { readCredentials } from '../../../src/fixtures/signin/credentials';
 import { Random } from '../../../src/libraries/tools/random';
+import { assert } from 'console';
 
 const testBase = new TestBase();
 const log = new Log();
+const random = new Random();
+const credentials = readCredentials();
 const baseUrl = 'https://www.google.com/';
+const subjectString = `This is a test ${random.Alphanumeric(6)}`;
 
 test.beforeAll(async ({ page }) => {
   log.StepDescription(`Navigate to ${baseUrl}.`);
@@ -17,6 +21,7 @@ test('Compose and send an email', async ({ page }) => {
   await navigateToSignInPage(page);
   await signInToGmail(page);
   await composeAndSendEmail(page);
+  await validateEmailIsDisplayed(page, true);
 });
 
 async function navigateToSignInPage(page) {
@@ -35,19 +40,17 @@ async function signInToGmail(page) {
   const signInButton = page.getByRole('link', { name: 'Sign in' });
   await signInButton.click();
 
-  const credentials = readCredentials();
-
   const emailAddressTextbox = page.getByRole('textbox', { name: 'Email or phone' });
-  await emailAddressTextbox.fill(credentials.username);
+  await emailAddressTextbox.fill(credentials.emailAddress);
 
-  let nextButton = page.getByRole('button', { name: 'Next' });
-  await nextButton.click();
+  const nextButtonEmail = page.locator('#identifierNext');
+  await nextButtonEmail.click();
 
   const passwordTextbox = page.getByRole('textbox', { name: 'Enter your password' });
   await passwordTextbox.fill(credentials.password);
 
-  nextButton = page.getByRole('button', { name: 'Next' });
-  await nextButton.click();
+  const nextButtonPassword = page.locator('#passwordNext')
+  await nextButtonPassword.click();
 }
 
 async function composeAndSendEmail(page) {
@@ -57,19 +60,41 @@ async function composeAndSendEmail(page) {
 
   const toEmailAddressTextbox = page.getByRole('combobox');
   await toEmailAddressTextbox.click();
-  await toEmailAddressTextbox.fill('bluemustardtest+abc@gmail.com');
+  await toEmailAddressTextbox.fill(credentials.emailAddress);
   await toEmailAddressTextbox.press('Tab');
-  
-  const random = new Random();
-  const subjectString = `This is a test - ${random.Alphanumeric(6)}`;
+
   const subjectTextbox = page.getByPlaceholder('Subject');
   await subjectTextbox.fill(subjectString);
-  
+
   log.StepDescription('Send the email.');
   const sendButton = page.getByRole('button', { name: 'Send ‪(Ctrl-Enter)‬' });
   await sendButton.click();
+  await expect(page.getByText('Message sent')).toHaveCount(1);
 }
 
-test.afterEach(async ({ browser }) => {
+async function validateEmailIsDisplayed(page, isDisplayed: boolean) {
+  log.StepDescription(`Validate email is ${isDisplayed ? 'received' : 'deleted'}.`);
+  const expectedEmailRow = page.getByRole('link', { name: subjectString });
+  await expect(expectedEmailRow).toHaveCount((isDisplayed ? 1 : 0));
+}
+
+async function clearInbox(page) {
+  log.StepDescription('Clear the inbox.');
+  const selectAllCheckbox = page.getByRole('button', { name: 'Select' }).getByRole('checkbox');
+  await selectAllCheckbox.click();
+
+  const deleteButton = page.getByRole('button', { name: 'Delete' });
+  await deleteButton.click();
+
+  await validateEmailIsDisplayed(page, false);
+  const confirmationMessage = page.locator(':text-matches("moved to Trash")');
+  await expect(confirmationMessage).toHaveCount(1);
+
+  const inboxMessage = page.getByText('Your Primary tab is empty.');
+  await expect(inboxMessage).toHaveCount(1);
+}
+
+test.afterEach(async ({ page, browser }) => {
+  await clearInbox(page);
   await testBase.Cleanup(browser);
 });
